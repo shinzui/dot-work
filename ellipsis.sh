04a128b45046247a0b223ee806fabebdd32f44e2
Nadeem Bitar <nadeem@gmail.com>
2017-07-10 15:49:48 -0700

    :tada: Initial commit

--- ellipsis.sh
+++ ellipsis.sh
@@ -3,9 +3,25 @@
 # shinzui/work ellipsis package
 
 # The following hooks can be defined to customize behavior of your package:
-# pkg.install() {
-#     fs.link_files $PKG_PATH
-# }
+pkg.install() {
+  mkdir -p ~/Gyehoeg/work
+
+  brew install terraform
+  install_app docker
+  install_app postico
+  yarn global add @topagentnetwork/tf-output @topagentnetwork/cloudwatch-logs-souji
+  yarn global add pino pino-colada plop
+}
+
+install_app() {
+  if test ! $(brew cask list |grep "$app"); then
+    echo "installing $app"
+    brew cask install --appdir="/Applications" $app
+  else
+    echo "$app is already installed. Skipping..."
+  fi
+}
+
 
 # pkg.push() {
 #     git.push
